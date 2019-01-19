ad2b36f16acfba1fdd9a559c241bf5030f303eb6
Manthan Shah <manthanshah1711@gmail.com>
2019-01-19 12:31:15 -0800

    Node Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import http = require('http');
+
+const hostname:string = '127.0.0.1';
+const port: number = 3000;
+
+
+
+const server = http.createServer(function(req, res)  {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  });
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
